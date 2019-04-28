0f1914a4d944abc3f3fa8a337d6caf8b636feb5a
ReiUtsuho <fxc1494@g.rit.edu>
2019-04-28 18:32:55 -0400

    Rename Folder

---
+++ Client/src/TCPClient.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class TCPClient
+{
+    private Socket socket = null;
+
+    private java.io.DataOutputStream socketOut = null;
+
+    private DataInputStream socketIn = null;
+
+    private BufferedReader input = null;
+
+
+
+
+    public TCPClient(String paramString, int paramInt)
+    {
+        int counter = 0;
+        try
+        {
+            socket = new Socket(paramString, paramInt);
+            socketOut = new java.io.DataOutputStream(socket.getOutputStream());
+            socketIn = new DataInputStream(new java.io.BufferedInputStream(socket.getInputStream()));
+            input = new BufferedReader(new java.io.InputStreamReader(System.in));
+        }
+        catch (java.net.ConnectException localConnectException)
+        {
+            System.out.println(localConnectException);
+            return;
+        }
+        catch (java.net.UnknownHostException localUnknownHostException)
+        {
+            System.out.println(localUnknownHostException);
+            return;
+        }
+        catch (IOException localIOException1)
+        {
+            System.out.println(localIOException1); return;
+        }
+        for (;;)
+        {
+            if (counter == 0) {
+                try
+                {
+                    String str = input.readLine();
+                    if (str.equals("end")) {
+                        counter = 1;
+                    }
+                    socketOut.writeUTF(str);
+                    str = socketIn.readUTF();
+                    System.out.println(str);
+                }
+                catch (java.io.EOFException localEOFException)
+                {
+                    System.out.println("Disconnected");
+                }
+                catch (IOException localIOException2)
+                {
+                    System.out.println(localIOException2);
+                }
+//                finally{
+//                    try
+//                    {
+//                        input.close();
+//                        socketOut.close();
+//                        socketIn.close();
+//                        socket.close();
+//                    }
+//                    catch (IOException localIOException3) {
+//                        System.out.println(localIOException3);
+//                    }
+//                }
+            }
+        }
+    }
+}
